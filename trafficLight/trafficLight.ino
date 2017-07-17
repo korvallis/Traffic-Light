7463d2061ef9c3698c688921ddf1e00db05bc65a
Kevin Kellar <kellar.kevin@gmail.com>
2017-07-17 03:00:50 +0100

    light switches with button press

--- trafficLight/trafficLight.ino
+++ trafficLight/trafficLight.ino
@@ -1,14 +1,74 @@
+
+const int PIN_RED = 2;
+const int PIN_YEL = 3;
+const int PIN_GRN = 4;
+
+
+int state = -1;
+
 void setup() 
 {
-  pinMode(7,OUTPUT);
+  
+  pinMode(PIN_RED,OUTPUT);
+  pinMode(PIN_YEL,OUTPUT);
+  pinMode(PIN_GRN,OUTPUT);
+  pinMode(5,INPUT);
+
+  digitalWrite(PIN_RED, HIGH);
 
 }
 
 void loop() 
 {
-   
-   digitalWrite(7,LOW);
-  delay(2000);
- digitalWrite(7,HIGH);
-  delay(2000);
+  
+      digitalWrite(PIN_RED,digitalRead(5));
+      /*
+      if(state == 0)
+      {
+          digitalWrite(PIN_RED, LOW);
+          digitalWrite(PIN_YEL, LOW);
+          digitalWrite(PIN_GRN, LOW);
+      }
+       if(state == 1)
+      {
+          digitalWrite(PIN_RED, HIGH);
+          digitalWrite(PIN_YEL, LOW);
+          digitalWrite(PIN_GRN, LOW);
+      }
+      if(state == 2)
+      {
+          digitalWrite(PIN_RED, LOW);
+          digitalWrite(PIN_YEL, HIGH);
+          digitalWrite(PIN_GRN, LOW);
+      }
+      if(state == 3)
+      {
+          digitalWrite(PIN_RED, LOW);
+          digitalWrite(PIN_YEL, LOW);
+          digitalWrite(PIN_GRN, HIGH);
+      }
+      if(state == 4)
+      {
+          digitalWrite(PIN_RED, HIGH);
+          digitalWrite(PIN_YEL, HIGH);
+          digitalWrite(PIN_GRN, HIGH);
+      }
+      if(state == 5)
+      {
+          if((( millis() /  900) % 2) == 0)
+           digitalWrite(PIN_RED, LOW);
+          else
+            digitalWrite(PIN_RED, HIGH);
+
+          
+          digitalWrite(PIN_YEL, LOW);
+          digitalWrite(PIN_GRN, LOW);
+      }
+
+    state = (int)  ((millis() / 1000.0));
+    state = state % 7;
+    
+  delay(5);
+  */
+  delay(10);
 }
